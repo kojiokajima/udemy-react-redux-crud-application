e1df531f78299acdc0e6146541039aeca61dc278
wanko <kouji124@w4.dion.ne.jp>
2020-12-29 12:10:48 -0800

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 // import logo from './logo.svg';
-import React, {Component} from 'react';
+// import React, {Component} from 'react';
 
 
 // function App() {
 //   return <h1>Hello World!</h1>
 // }
 
-class App extends Component {
-  render() {
-    return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() => {console.log("I am clicked")}} />;
-    </React.Fragment>
+// class App extends Component {
+//   render() {
+//     return (
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onChange={() => {console.log("I am clicked")}} />;
+//     </React.Fragment>
 
-    );
-  }
+//     );
+//   }
+// }
+
+const App = () => {
+  return (
+  <div>
+    <Cat />
+    <Cat />
+    <Cat />
+    <Cat />
+  </div>
+  )
+}
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 
